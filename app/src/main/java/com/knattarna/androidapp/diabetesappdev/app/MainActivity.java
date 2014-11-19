95c3081f0274141814b83b40ff9db22b6b43188c
Nattmaran <jonmo578@student.liu.se>
2014-11-19 23:47:20 +0100

    made own ArrayAdapter(extended) class, this makes it so that we can load custom views as items

--- app/src/main/java/com/knattarna/androidapp/diabetesappdev/app/MainActivity.java
+++ app/src/main/java/com/knattarna/androidapp/diabetesappdev/app/MainActivity.java
@@ -1,5 +1,6 @@
 package com.knattarna.androidapp.diabetesappdev.app;
 
+import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -12,6 +13,8 @@ import android.view.ViewGroup;
 import android.os.Build;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.util.List;
 
 import java.util.ArrayList;
 
@@ -56,18 +59,59 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+
+    /**
+    *   Extending the ArrayAdapter to override getView() method
+    *   for custom views.
+    */
+
+    //todo change List<String> to Objects[] objects for Activity Objects or smthing
+
+   public static class ActivityAdapter extends ArrayAdapter {
+
+        private Context context;
+        private int resourceid;
+        private int layoutid;
+        private List<String> objects;
+
+        public ActivityAdapter (Context ctext, int layoutid, int resourceid, List<String> objects) {
+            super(ctext, layoutid, resourceid, objects);
+
+            this.context = ctext;
+            this.resourceid = resourceid;
+            this.layoutid = layoutid;
+            this.objects = objects;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent)
+        {
+            View row = convertView; //current list-item maybe Iduntknow
+            LayoutInflater inflater = ((android.app.Activity) this.context).getLayoutInflater();
+
+            if (row == null)
+            {
+                row = inflater.inflate(this.layoutid, parent, false);
+            }
+
+            return row;
+        }
+    }
+
     /**
      * A placeholder fragment containing a simple view.
      */
     public static class PlaceholderFragment extends Fragment {
 
         public PlaceholderFragment() {
+            //super();
         }
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
             ArrayList<String> meals = new ArrayList<String>() {{
                 add("Breakfast");
                 add("Brunch");
@@ -85,11 +129,13 @@ public class MainActivity extends ActionBarActivity {
                 add("Snack");
                 add("Dinner");
             }};
-            ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+
+            ActivityAdapter adapter = new ActivityAdapter(
                     getActivity(),
                     R.layout.list_item_meal,
                     R.id.list_item_meal,
                     meals);
+
             ListView list = (ListView) rootView.findViewById(R.id.listView_meals);
             list.setAdapter(adapter);
             return rootView;
